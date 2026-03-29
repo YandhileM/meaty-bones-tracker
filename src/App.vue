6c8a1b2ba3c4294828e18e1189d8653ef4acdb2b
Yandhile <yandhilemaluleke@gmail.com>
2026-03-29 14:34:25 +0200

    feat: add nav drawer, app bar, logout, and router integration

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
-import { watch } from 'vue'
+import { computed, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 import { useTheme } from 'vuetify'
 import { useThemeStore } from './stores/theme.js'
+import { clearAuth } from './services/auth.js'
 
+const route = useRoute()
+const router = useRouter()
 const vuetifyTheme = useTheme()
 const themeStore = useThemeStore()
+
+const isLoginPage = computed(() => route.path === '/login')
 
 watch(
   () => themeStore.isDark,
@@ -13,19 +19,52 @@ watch(
   },
   { immediate: true }
 )
+
+function logout() {
+  clearAuth()
+  router.push('/login')
+}
 </script>
 
 <template>
   <v-app>
-    <v-main>
-      <v-container class="d-flex justify-center align-center" style="min-height: 100vh">
+    <v-navigation-drawer v-if="!isLoginPage" permanent>
+      <v-list-item title="Meaty Bones" subtitle="Credit Tracker" class="py-4" />
+      <v-divider />
+
+      <v-list nav density="compact">
+        <v-list-item prepend-icon="mdi-view-dashboard" title="Dashboard" to="/" />
+      </v-list>
+
+      <template #append>
+        <v-divider />
+        <v-list nav density="compact">
+          <v-list-item
+            prepend-icon="themeStore.isDark ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+            :title="themeStore.isDark ? 'Light Mode' : 'Dark Mode'"
+            @click="themeStore.toggle()"
+          />
+          <v-list-item
+            prepend-icon="mdi-logout"
+            title="Logout"
+            @click="logout"
+          />
+        </v-list>
+      </template>
+    </v-navigation-drawer>
+
+    <v-app-bar v-if="!isLoginPage" elevation="1">
+      <v-app-bar-title>Meaty Bones Tracker</v-app-bar-title>
+      <template #append>
         <v-btn
-          :prepend-icon="themeStore.isDark ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          :icon="themeStore.isDark ? 'mdi-weather-sunny' : 'mdi-weather-night'"
           @click="themeStore.toggle()"
-        >
-          {{ themeStore.isDark ? 'Switch to Light' : 'Switch to Dark' }}
-        </v-btn>
-      </v-container>
+        />
+      </template>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
     </v-main>
   </v-app>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createVuetify } from 'vuetify'
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import App from './App.vue'
+import router from './router/index.js'
 
 const vuetify = createVuetify({
   theme: {
@@ -14,4 +15,5 @@ const vuetify = createVuetify({
 const app = createApp(App)
 app.use(createPinia())
 app.use(vuetify)
+app.use(router)
 app.mount('#app')
